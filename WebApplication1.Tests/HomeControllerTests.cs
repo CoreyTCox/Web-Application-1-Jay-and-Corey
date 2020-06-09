ac3a76651936612798315ca9911296ef45c54091
Corey T Cox <coxcorey19@gmail.com>
2020-06-09 13:04:49 -0400

    initial commit

---
+++ WebApplication1.Tests/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplication1.Controllers;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public string Index()
+        //{
+        //    return "Hello World!";
+        //}
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
